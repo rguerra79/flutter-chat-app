c7ad163af18d2df35569942f3b62090b520f2e86
Roberto Guerra <rguerra79@gmail.com>
2023-08-18 11:00:46 -0400

    Fin seccion 7

--- lib/pages/usuarios_page.dart
+++ lib/pages/usuarios_page.dart
@@ -1,12 +1,83 @@
+import 'package:chat/models/usuario.dart';
 import 'package:flutter/material.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
-class UsuariosPage extends StatelessWidget {
+class UsuariosPage extends StatefulWidget {
+  @override
+  State<UsuariosPage> createState() => _UsuariosPageState();
+}
+
+class _UsuariosPageState extends State<UsuariosPage> {
+  RefreshController _refreshController =
+      RefreshController(initialRefresh: false);
+  final usuarios = [
+    Usuario(uid: '1', nombre: 'Lucas', email: 'test1@test.com', online: false),
+    Usuario(uid: '2', nombre: 'Alana', email: 'test1@test.com', online: true),
+    Usuario(uid: '3', nombre: 'Milo', email: 'test1@test.com', online: false),
+    Usuario(uid: '4', nombre: 'Olivia', email: 'test1@test.com', online: true),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('UsuariosPage'),
+        appBar: AppBar(
+          title: const Text(
+            'Mi Nombre',
+            style: TextStyle(color: Colors.black87),
+          ),
+          elevation: 1,
+          backgroundColor: Colors.white,
+          leading: IconButton(
+            icon: const Icon(Icons.exit_to_app, color: Colors.black87),
+            onPressed: () {},
+          ),
+          actions: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(right: 10),
+              child: Icon(Icons.check_circle, color: Colors.blue[400]),
+            )
+          ],
+        ),
+        body: SmartRefresher(
+          controller: _refreshController,
+          enablePullDown: true,
+          onRefresh: _cargarUsuarios,
+          header: WaterDropHeader(
+            complete: Icon(Icons.check, color: Colors.green[400]),
+            waterDropColor: Colors.blue,
+          ),
+          child: _listViewUsuarios(),
+        ));
+  }
+
+  ListView _listViewUsuarios() {
+    return ListView.separated(
+      physics: const BouncingScrollPhysics(),
+      itemBuilder: (_, i) => _usuarioTile(usuarios[i]),
+      itemCount: usuarios.length,
+      separatorBuilder: (_, int index) => const Divider(),
+    );
+  }
+
+  ListTile _usuarioTile(Usuario usuario) {
+    return ListTile(
+      title: Text(usuario.nombre),
+      subtitle: Text(usuario.email),
+      leading: CircleAvatar(
+        child: Text(usuario.nombre.substring(0, 2)),
+        backgroundColor: usuario.online ? Colors.blue[100] : Colors.red[100],
+      ),
+      trailing: Container(
+        width: 10,
+        height: 10,
+        decoration: BoxDecoration(
+            color: usuario.online ? Colors.green[300] : Colors.red,
+            borderRadius: BorderRadius.circular(100)),
       ),
     );
+  }
+
+  _cargarUsuarios() async {
+    await Future.delayed(const Duration(milliseconds: 1000));
+    _refreshController.refreshCompleted();
   }
 }
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,4 +1,5 @@
 export 'package:chat/routes/labels.dart';
 export 'package:chat/routes/logo.dart';
 export 'package:chat/widgets/boton_azul.dart';
+export 'package:chat/widgets/chat_message.dart';
 export 'package:chat/widgets/custom_input.dart';
